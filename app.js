f64530ecb760dc23bff2ac772d027ab32adb33db
Yazan70 <titiyazan14@gmail.com>
2023-05-31 18:42:15 +0300

    update

--- app.js
+++ app.js
@@ -6,3 +6,41 @@ const mycar="KIA"
 car=["bmw","benz","opel"]
 console.log(car)
 console.log(myname,"@",mymajor,"@",mycar)
+
+
+let myName="yazan"
+
+for (let i = 0; i < 10; i++) {
+    console.log(myName);
+    
+}
+
+let mark = prompt("enter your mark")
+
+if (mark>0 && mark<49) {
+    console.log("failed")
+    
+}
+
+else if (mark<50 && mark>60) {
+    console.log("D")
+}
+else if (mark<61 && mark>70) {
+    console.log("c")
+}
+
+else if (mark<71 && mark>80) {
+    console.log("B")
+}
+else if (mark<81 && mark>90) {
+    console.log("A")
+}
+
+else {
+    console.log("A+")
+}
+
+
+
+
+
